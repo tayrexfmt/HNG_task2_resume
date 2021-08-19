f37d575fae5d0b5054dd0a5877167cf0581aac6f
tayrexfmt <tayrexfmt@gmail.com>
2021-08-19 19:29:37 +0100

    new

--- HNG_resume_task2/wsgi.py
+++ HNG_resume_task2/wsgi.py
@@ -12,14 +12,14 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-from HNG_resume_task2 import MyWSGIApp
+#from HNG_resume_task2 import MyWSGIApp
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HNG_resume_task2.settings')
 
 application = get_wsgi_application()
 
 
-application = MyWSGIApp()
+#application = MyWSGIApp()
 application = WhiteNoise(application, root='/path/to/static/files')
 application.add_files('/path/to/more/static/files', prefix='more-files/')
 
--- resume_app/models.py
+++ resume_app/models.py
@@ -5,7 +5,7 @@ class feature(models.Model):
     #id: int
     usernamename = models.CharField(max_length=100)
     email = models.CharField(max_length=500)
-    phonenumber = models.IntegerField(max_length=15)
+    phonenumber = models.IntegerField()
     address = models.CharField(max_length=500)
     comment = models.CharField(max_length=500)
     
