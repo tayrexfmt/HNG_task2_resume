207440d282c0ebda964b933ba79ab8552bb39ad6
tayrexfmt <tayrexfmt@gmail.com>
2021-08-19 12:29:51 +0100

    Postgres database migration

--- HNG_resume_task2/settings.py
+++ HNG_resume_task2/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'HNG_resume_task2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Resume',
+        'USER' : 'postgres',
+        'PASSWORD' : 'admin8293',
+        'HOST' : 'localhost' 
     }
 }
 
